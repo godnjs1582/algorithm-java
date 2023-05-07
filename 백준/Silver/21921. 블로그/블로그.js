cc918c903663f8e81c940c506394d0e466f7c368
HaewonKwon(pinky) <101165990+godnjs1582@users.noreply.github.com>
2023-05-07 21:01:41 +0900

    [Silver III] Title: 블로그, Time: 280 ms, Memory: 34048 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.js
+++ 백준/Silver/21921. 블로그/블로그.js
@@ -1,43 +1,39 @@
-
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let [n,x]= input[0].split(" ").map(Number)
-let arr=[0,...input[1].split(" ").map(Number)]
-//n=7
-//x=5
-//arr=[0,1,1,1,1,1,5,1]
-
-let sum = 0;
-for(let i=1;i<=n;i++){
-  //1부터 x번째 날의 방문자 수의 합
-  if(i<=x) sum=sum+arr[i]
+let [n,x]=input[0].split(" ").map(Number)
+//n=5
+//x=2
+let arr=input[1].split(" ").map(Number)
+let sum=0;
+for(i=0;i<n;i++){
+  if(i<x) sum=sum+arr[i]
 }
-let maxSum=sum; //가장 큰 합
-let count=1;//기간의 개수
+//초기값 5
+let start=0;
+let end=x;
+let count=1;
+let maxSum=sum;
 
-
-// 슬라이딩 윈도우 시작
-let left = 1;
-let right = x;
-
-while(right+1<=n){//윈도우를 한 칸 오르쪽으로 이동하기
-  left++
-  right++
-  sum=sum+arr[right]-arr[left-1]
-  if(sum===maxSum) count++
-  else if(maxSum<sum){
-    maxSum=sum
-    count=1
+while(end<n){
+  sum =sum+arr[end++]-arr[start++]
+  if(maxSum===sum){
+    count++
   }
+
+  if(maxSum<sum){
+    count=1;
+    maxSum=sum
+  }
+
+
 }
 
-if(maxSum===0) console.log("SAD")
-else{
-  console.log(maxSum)
-  console.log(count)
+if(sum===0){
+  console.log("SAD")
+}else{
+  console.log(maxSum+"\n"+count)
 }
-
 
 
 
