5e8c752ad86a3f99390e9eca092877e9f4f10e57
haewon kwon <101165990+godnjs1582@users.noreply.github.com>
2023-10-22 17:11:59 +0900

    [level 2] Title: 타겟 넘버, Time: 2.13 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,15 @@
+function solution(numbers, target) {
+    var answer = 0;
+    function DFS(L,sum){
+       if(L===numbers.length){
+           if(sum===target){
+               answer++
+           }
+       }else{
+           DFS(L+1,sum+numbers[L])
+           DFS(L+1,sum-numbers[L])
+       }
+    }
+    DFS(0,0);
+    return answer;
+}
